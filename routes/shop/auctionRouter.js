b17da65358ea186b56ff620d97823b0bdfa9f91a
minkyong00 <minkyong131@gmail.com>
2025-04-09 19:29:49 +0900

    최종 입찰 시 auction 컬렉션에 입찰 정보 배열로 저장

--- routes/shop/auctionRouter.js
+++ routes/shop/auctionRouter.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getAuctionProduct, getAuctionProductById, seedAuctionProducts } from '../../controller/shop/auctionController.js';
+import passport from 'passport';
+import { createBidCount, getAuctionProduct, getAuctionProductById, seedAuctionProducts } from '../../controller/shop/auctionController.js';
 
 const auctionRouter = express.Router();
 
 auctionRouter.get("/", getAuctionProduct) // /shop/auction (AuctionMain)
 auctionRouter.get("/detail/:id", getAuctionProductById) // /shop/auction/detail/1 (AuctionDetail)
+auctionRouter.put("/bid/:id", passport.authenticate('jwt', { session : false }), createBidCount) // /shop/auction/bid/:id (AuctionBidContainer)
 
 export default auctionRouter;
